ae80cf7fe3a1334508fb6683e1df7e63b7e937d6
Paulo Agapito <111472748+Zenn1412@users.noreply.github.com>
2022-09-12 18:00:42 +0800

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,136 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class Calculator implements ActionListener {
+
+    JFrame frame;
+    JTextField textfield;
+    JButton[] numberButtons = new JButton[10];
+    JButton[] functionButtons = new JButton[9];
+
+    JButton addButton, subButton, mulButton, divButton;
+    JButton decButton, equButton, delButton, clrButton, negButton;
+
+    JPanel panel;
+    Font myFont = new Font("Segoe UI", Font.PLAIN, 30);
+    Font ledFont = new Font("Segoe UI", Font.PLAIN, 38);
+    
+    double num1 = 0, num2 = 0, result = 0;
+    char operator;
+
+    Calculator() {
+        // initialize the frame 
+        frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420,550);
+        frame.setLayout(null);
+
+        textfield  = new JTextField();
+        textfield.setBounds(50, 15, 300, 80);
+        textfield.setFont(myFont);
+        textfield.setBackground(Color.BLACK);
+        textfield.setForeground(Color.GREEN);
+        textfield.setEditable(false);
+
+        addButton = new JButton("+");
+        subButton = new JButton("-");
+        mulButton = new JButton("*");
+        divButton = new JButton("/");
+        decButton = new JButton(".");
+        equButton = new JButton("=");
+        delButton = new JButton("DEL");
+        clrButton = new JButton("CLR");
+        negButton = new JButton("(-)");
+
+        functionButtons[0] = addButton;
+        functionButtons[1] = subButton;
+        functionButtons[2] = mulButton;
+        functionButtons[3] = divButton;
+        functionButtons[4] = decButton;
+        functionButtons[5] = equButton;
+        functionButtons[6] = delButton;
+        functionButtons[7] = clrButton;
+        functionButtons[8] = negButton;
+
+        for (int i = 0; i <9; i++) {
+            functionButtons[i].addActionListener(this);
+            functionButtons[i].setFont(myFont);
+            functionButtons[i].setFocusable(false);
+        }
+
+        for (int i=0; i < 10; i++) {
+            numberButtons[i] = new JButton(String.valueOf(i));
+            numberButtons[i].addActionListener(this);
+            numberButtons[i].setFont(myFont);
+            numberButtons[i].setFocusable(false);
+        }
+
+        negButton.setBounds(50,430,100,50);
+        delButton.setBounds(150,430,100,50);
+        clrButton.setBounds(250,430,100,50);
+
+        // create the panel
+        panel = new JPanel();
+        panel.setBounds(50,115,300,300);
+        panel.setLayout(new GridLayout(4,4,10,10));
+
+        panel.add(numberButtons[1]);
+        panel.add(numberButtons[2]);
+        panel.add(numberButtons[3]);
+        panel.add(addButton);
+
+        panel.add(numberButtons[4]);
+        panel.add(numberButtons[5]);
+        panel.add(numberButtons[6]);
+        panel.add(subButton);
+
+        panel.add(numberButtons[7]);
+        panel.add(numberButtons[8]);
+        panel.add(numberButtons[9]);
+        panel.add(mulButton);
+        panel.add(decButton);
+        panel.add(numberButtons[0]);
+        panel.add(equButton);
+        equButton.setBackground(Color.ORANGE);
+        panel.add(divButton);
+
+        frame.add(delButton);
+        frame.add(clrButton);
+        frame.add(negButton);
+
+        frame.add(textfield);
+
+        frame.add(panel);
+
+        frame.setVisible(true);
+
+    }
+      
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // for number buttons
+        for (int i =0; i <10; i++) {
+            if (e.getSource() == numberButtons[i]) {
+                textfield.setText(textfield.getText().concat(String.valueOf(i)));
+            }
+        }
+        // the decimal button
+        if (e.getSource()==decButton) {
+            textfield.setText(textfield.getText().concat("."));
+        }
+        // the add button
+
+        if (e.getSource()== addButton) {
+            num1 = Double.parseDouble(textfield.getText());
+            operator = '+';
+            textfield.setText("");
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Calculator calc = new Calculator();
+    }
+
+}
