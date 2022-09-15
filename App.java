95d4ac1483187c65bbaef89f805d218a626262bc
Paulo Agapito <111472748+Zenn1412@users.noreply.github.com>
2022-09-15 23:34:10 +0800

    Add files via upload

--- App.java
+++ App.java
@@ -1,58 +1,24 @@
+
+// import the Scanner class
 import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        int ave;
 
-        ave = 75;
-        if (ave > 74 ) {
-            System.out.println( " You passed! ");
-        }
-        else {
-            System.out.println( " Sorry you failed!");
-        }
-       
-        String psswd = "Secret101";
+    // Scanner Object
+    static Scanner scan = new Scanner(System.in);
 
-        if (psswd != "Secret101 ") {
-            System.out.println(" Password accepted");
-        }
-   
-        float gwa;
-        System.out.println("type Average ");
-        Scanner x= new Scanner(System.in);
-        gwa = x.nextFloat();
-        System.out.println(gwa);
-
-        if (gwa > 89 && gwa < 92); {
-            System.out.println(" Cum Laude ");
-        }  if (gwa > 92 && gwa < 97) {
-            System.out.println("  Magna Cum Laude ");
-        }
-          else if (gwa > 96 && gwa < 101) {
-            System.out.println(" Suma Cum Laude ");
-          } else if (gwa < 88) {
-            System.out.println(" No Honors for you ");
-          } else if (gwa > 100) {
-            System.out.println(" Invalid grade ");
-          }
-
-        if (gwa > 75 || gwa < 70) {
-            System.out.println(" Valid Grade ");
-        } else {
-            System.out.println(" Invalid grade ");
-        }
-   
-    }
-}
-
-
-
-
+    public static void main(String[] args) {
     
 
+   String crushname = getnameofCrush();
+   System.out.println(crushname);
+}
+static String getnameofCrush() {
+   System.out.println("this will show your crush name: ");
+   System.out.print("Enter the name of your crush: ");
 
-
-
-
-
-
+   String crushName = scan.nextLine();
+   return crushName;
+   
+}
+   }
