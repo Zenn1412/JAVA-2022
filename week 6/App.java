920e7305cc27513c211096828d251d6ea35b1612
Paulo Agapito <111472748+Zenn1412@users.noreply.github.com>
2022-09-19 10:56:29 +0800

    Add files via upload

---
+++ week 6/App.java
@@ -0,0 +1,33 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        String today = "asdffa";
+
+       switch (today) {
+        case "monday":
+        System.out.println("I'm studying in mondays");
+        break;
+        case "tuesday":
+        System.out.println("I am playing bastketball at tuesdays");
+        break;
+        case "wednesday":
+        System.out.println("I am going to school at wednesday");
+        break;
+        case "thursday":
+        System.out.println("I am watching movies at thursday");
+        break; 
+        case "friday":
+        System.out.println("asdfasldkf");
+        break;
+        case "saturday":
+        System.out.println("afasdl;faskdf");
+        break;
+        case "sunday":
+        System.out.println("sunday morning");
+        break;
+        default: System.out.println("N/A");
+        break;
+        }
+
+    }
+}
